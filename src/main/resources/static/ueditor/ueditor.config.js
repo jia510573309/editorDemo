48ea69b38c910d1077fdc2ae75435cdb97e7ed7f
Jia Min <510573309@qq.com>
2018-10-09 09:26:17 +0800

    config.js

--- src/main/resources/static/ueditor/ueditor.config.js
+++ src/main/resources/static/ueditor/ueditor.config.js
@@ -30,7 +30,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "http://192.168.0.200:8080/img/ueditor"
+        , serverUrl: "http://192.168.0.200:8082/img/ueditor"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
